1d4471b66b6b9a84c477d421257f5d0b0b734ddb
Clairezy <clairezwicker98@gmail.com>
2023-01-15 15:15:54 -0600

    Initial Commit

---
+++ Homework 1/Homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_1App.swift
+//  Homework 1
+//
+//  Created by Claire on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
